9b09800f50ba1aabb4c9e947a2fc7cbc32e34045
Jake <jake@syntasso.io>
2024-01-05 10:34:12 +0000

    fix: update ginkgo

--- lib/generator_test.go
+++ lib/generator_test.go
@@ -6,7 +6,7 @@ import (
 	"path/filepath"
 
 	"github.com/aclevername/kratix-backstage-generator-pipeline/lib"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
